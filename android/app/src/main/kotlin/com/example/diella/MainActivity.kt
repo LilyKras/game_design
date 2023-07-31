491e92735e9fba4abbb361a01af1d780c71e9fb5
LilyKras <62222363+LilyKras@users.noreply.github.com>
2023-07-31 17:03:38 +0300

    First commit

---
+++ android/app/src/main/kotlin/com/example/diella/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.diella
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+
+import 'package:diella/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+
+Future<void> _init() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterError(errorDetails);
+  };
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+}
+
+void main() {
+  _init();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(body: Text('AAA')),
+      routes: const {
+            // MainScreen.routeName: (ctx) => const MainScreen(),
+            // NewTaskScreen.routeName: (ctx) => const NewTaskScreen(),
+          },
+    );
+  }
+}
+
+
